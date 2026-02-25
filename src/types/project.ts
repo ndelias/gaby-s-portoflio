e875099ec50c3a0fb5453a778a5f87c50f577cf8
ndelias <ndelias@usc.edu>
2026-02-25 14:17:49 -0800

    added images for the journey and removed a project

--- src/types/project.ts
+++ src/types/project.ts
@@ -12,7 +12,7 @@ export interface ProjectImage {
   alt: string;
   width: number;
   height: number;
-  layout?: "full" | "half" | "third";
+  layout?: "full" | "half" | "third" | "tall-left";
   title?: string;
   rotate?: number;
 }
